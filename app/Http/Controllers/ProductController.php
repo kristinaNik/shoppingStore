9ebc0ea735b9410d96073dac9b0d4291f1946428
kristinaNik <kristina.nik93@gmail.com>
2019-09-20 17:02:37 +0300

    Modified the code in controller to use the session from the request object

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,8 +8,6 @@ use App\Http\Resources\Product as ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
-
 
 class ProductController extends Controller
 {
@@ -23,32 +21,8 @@ class ProductController extends Controller
         $searchField = $request->get('search');
         $products = Product::search($searchField);
 
-      //  return view('shop/index', ['products' => $products]);
-
-
         return new ProductCollection(collect($products));
 
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
     }
 
     /**
@@ -64,42 +38,15 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Store items to cart
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
-
     public function getAddToCart(Request $request, $id) {
         $products = Product::findOrFail($id);
-        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $oldCart = $request->session()->has('cart') ? $request->session()->get('cart') : null;
         $cart = new Cart($oldCart);
         if (Auth::check()) {
             $cart->add($products, $products->id);
@@ -113,12 +60,14 @@ class ProductController extends Controller
     }
 
     /**
+     * Reduce items in cart by one
+     *
      * @param Request $request
      * @param $id
      * @return \Illuminate\Http\RedirectResponse
      */
     public function getReduceByOne(Request $request, $id) {
-        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $oldCart = $request->session()->has('cart') ? $request->session()->get('cart') : null;
         $cart = new Cart($oldCart);
         if (Auth::check()) {
             $cart->reduceByOne($id);
@@ -137,8 +86,15 @@ class ProductController extends Controller
         }
     }
 
+    /**
+     * Remove items from cart
+     *
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function getRemoveItem(Request $request, $id) {
-        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        $oldCart = $request->session()->has('cart') ? $request->session()->get('cart') : null;
             $cart = new Cart($oldCart);
             $cart->removeItem($id);
 
@@ -151,13 +107,15 @@ class ProductController extends Controller
     }
 
     /**
+     * Get the shopping cart
+     *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function getCart() {
-        if (!Session::has('cart')) {
+    public function getCart(Request $request) {
+        if (!$request->session()->has('cart')) {
             return view('shop.shopping-cart');
         }
-        $oldCart = Session::get('cart');
+        $oldCart = $request->session()->get('cart');
         $cart = new Cart($oldCart);
 
         return view('shop.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
