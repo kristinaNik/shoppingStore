b2bd713dd6468572b13a378621bff5e6c4d605cc
kristinaNik <kristina.nik93@gmail.com>
2020-01-18 22:21:15 +0200

    Modified the DatabaseSeeder and the users table migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-       // $this->call(ProductSeeder::class);
-        factory(App\Product::class, 20)->create();
+       // factory(App\Product::class, 2)->create();
+        factory(\App\Role::class)->create();
     }
 }
